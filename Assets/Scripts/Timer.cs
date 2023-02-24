53624ab934def4f1f2a91d206bc1545e25476bbb
jessicaduque <69450467+jessicaduque@users.noreply.github.com>
2023-02-24 09:18:18 -0300

    Timer

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Timer : MonoBehaviour
+{
+    float segundos = 1f;
+    int minutos = 4;
+    bool acabouTempo = false;
+    bool comecouTimer = false;
+
+    public GameObject acabouTempoPanel;
+
+    public Text countdown;
+
+    void Start()
+    {
+        countdown.text = "Tempo: 04:00";
+    }
+
+    private void Update()
+    {
+        if (acabouTempo)
+        {
+            segundos = 0;
+            countdown.text = "Tempo: 00:00";
+            GameObject.FindGameObjectWithTag("Canvas").GetComponent<ControleCanvas>().DesfixarMousePrenderPers();
+            acabouTempoPanel.SetActive(true);
+        }
+        else
+        {
+            if (comecouTimer)
+            {
+                if (minutos == 0 && segundos <= 0)
+                {
+                    acabouTempo = true;
+                }
+                else if (segundos <= 0)
+                {
+                    segundos = 59f;
+                    minutos--;
+                }
+
+                segundos -= 1 * Time.deltaTime;
+
+                if (segundos < 10 && minutos < 10)
+                {
+                    countdown.text = "Tempo: " + "0" + minutos.ToString() + ":0" + segundos.ToString("0");
+                }
+                else if (segundos < 10)
+                {
+                    countdown.text = "Tempo: " + minutos.ToString() + ":0" + segundos.ToString("0");
+                }
+                else if (minutos < 10)
+                {
+                    countdown.text = "Tempo: " + "0" + minutos.ToString() + ":" + segundos.ToString("0");
+                }
+                else
+                {
+                    countdown.text = "Tempo: " + minutos.ToString() + ":" + segundos.ToString("0");
+                }
+            }
+            else
+            {
+                segundos -= 1 * Time.deltaTime;
+                if (segundos < 0)
+                {
+                    segundos = 59;
+                    minutos--;
+                    comecouTimer = true;
+                }
+            }
+        }
+    }
+
+    public int TempoGasto()
+    {
+        return 4 - minutos;
+    }
+}
